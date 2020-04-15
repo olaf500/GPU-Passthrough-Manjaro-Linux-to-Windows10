d8feb597e91dbebe0d11da8023a9ab83dff98350
olaf500 <olaf500@users.noreply.github.com>
2020-04-15 13:53:22 -0400

    Update select-gpu.sh

--- select-gpu.sh
+++ select-gpu.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+## load kernel parameters
 full_cmd=$(cat /proc/cmdline) 
+
+## trim all before XORGCONFIG=
 trimmed_cmd=${full_cmd#*XORGCONFIG=}
-#echo $trimmed_cmd
+
+## trim all after the first space including the space
 xorg_conf_path=${trimmed_cmd%% *}
+
+## create symlink in the standard location
 echo $xorg_conf_path
 ln -s -f /etc/X11/$xorg_conf_path /etc/X11/xorg.conf
